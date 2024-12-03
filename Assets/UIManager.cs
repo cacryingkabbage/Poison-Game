af3354497481726c5b4deefcc3304625117e8bed
cacryingkabbage <167156462+cacryingkabbage@users.noreply.github.com>
2024-12-04 00:09:15 +0800

    .
    Unnecessary code I forgot to remove

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,1 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Threading;
+using System.Timers;
 
+// NOTE: this is a reference file. not actually used for the game.
+
+public class UImanager : MonoBehaviour
+{
+    public int healthValue;
+    public TextMeshProUGUI healthValueText;
+    public TextMeshProUGUI stamValueText;
+    public int stamValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stamValueText.text = stamValue.ToString();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        healthValueText.text = healthValue.ToString();
+    }
+    public void DamagePlayer()
+    {
+        healthValue -= 300;
+    }
+    public void poisonPlayer()
+    {
+        healthValue -= 50;
+    }
+    public void healPlayer()
+    {
+        healthValue += 200;
+    }
+}
